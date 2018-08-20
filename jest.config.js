8a2a641b607bcb93d3989a8e3483975975ff59d7
Caio Uvini <caio.uvini@movile.com>
2018-08-20 12:12:06 -0300

    simplifying jest config regex

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const {defaults} = require('jest-config');
 module.exports = {
     moduleDirectories: [...defaults.moduleDirectories, 'src'],
     moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
-    testRegex: '(/__tests__/.*|(\\.|/)test)\\.(jsx?|tsx?)$',
+    testRegex: '(/__tests__/.*\\.test)\\.(jsx?|tsx?)$',
     transform: {
         '^.+\\.jsx?$': 'babel-jest',
         '^.+\\.tsx?$': 'ts-jest'
