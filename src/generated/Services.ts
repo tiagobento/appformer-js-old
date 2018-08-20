4aef90ebbcd0ea893fdd7628807ad6fb11d1fa41
Caio Uvini <caio.uvini@movile.com>
2018-08-20 18:56:56 -0300

    fix triple equals linter complaint

--- src/generated/Services.ts
+++ src/generated/Services.ts
@@ -2,7 +2,7 @@ import { RPC } from "core";
 import { TestEvent, ErraiBusObject, Portable } from "generated/Model";
 
 function marshall(obj: Portable<any> & ErraiBusObject | string) {
-  return typeof obj != "string" ? obj.__toErraiBusObject().__toJson() : obj;
+  return typeof obj !== "string" ? obj.__toErraiBusObject().__toJson() : obj;
 }
 
 // TODO: Implement to return an object from the Model file
@@ -10,6 +10,7 @@ function unmarshall(json: any) {
   return json;
 }
 
+/* tslint:disable */
 export namespace Services {
   export namespace org {
     export namespace uberfire {
