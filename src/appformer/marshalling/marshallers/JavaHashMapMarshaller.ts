f7b4806a33f42bf3872f65d6479a90bb362db6ab
Caio Uvini <caio.uvini@movile.com>
2018-09-06 18:56:41 -0300

    fix(marshalling): use generic types for hash map

--- src/appformer/marshalling/marshallers/JavaHashMapMarshaller.ts
+++ src/appformer/marshalling/marshallers/JavaHashMapMarshaller.ts
@@ -3,8 +3,8 @@ import JavaHashMap from "../../java-wrappers/JavaHashMap";
 import MarshallingContext from "../MarshallingContext";
 import ErraiObject from "../model/ErraiObject";
 
-export default class JavaHashMapMarshaller implements Marshaller<JavaHashMap, ErraiObject> {
-  public marshall(input: JavaHashMap, ctx: MarshallingContext): ErraiObject {
+export default class JavaHashMapMarshaller<T, U> implements Marshaller<JavaHashMap<T, U>, ErraiObject> {
+  public marshall(input: JavaHashMap<T, U>, ctx: MarshallingContext): ErraiObject {
     // TODO
     return {};
   }
