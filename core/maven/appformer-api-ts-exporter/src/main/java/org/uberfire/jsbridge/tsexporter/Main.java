e081be63f17d1901f1f93c5515cdd94ce70ff503
Tiago Bento <tiagobentofernandes@gmail.com>
2018-09-14 15:42:45 -0300

    Reorganizing

---
+++ core/maven/appformer-api-ts-exporter/src/main/java/org/uberfire/jsbridge/tsexporter/Main.java
@@ -0,0 +1,127 @@
+package org.uberfire.jsbridge.tsexporter;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+
+import org.uberfire.jsbridge.tsexporter.decorators.DecoratorImportEntry;
+
+import static java.lang.Boolean.getBoolean;
+import static java.util.Arrays.asList;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toMap;
+import static javax.tools.Diagnostic.Kind.ERROR;
+import static javax.tools.StandardLocation.CLASS_OUTPUT;
+import static org.uberfire.jsbridge.tsexporter.Main.ENTRY_POINT;
+import static org.uberfire.jsbridge.tsexporter.Main.PORTABLE;
+import static org.uberfire.jsbridge.tsexporter.Main.REMOTE;
+
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@SupportedAnnotationTypes({REMOTE, PORTABLE, ENTRY_POINT})
+public class Main extends AbstractProcessor {
+
+    static final String REMOTE = "org.jboss.errai.bus.server.annotations.Remote";
+    static final String PORTABLE = "org.jboss.errai.common.client.api.annotations.Portable";
+    static final String ENTRY_POINT = "org.jboss.errai.ioc.client.api.EntryPoint";
+    static final String TS_EXPORTER_PACKAGE = "org.appformer.tsexporter.exports";
+
+    public static Types types;
+    public static Elements elements;
+
+    private static final List<Element> seenPortables = new ArrayList<>();
+    private static final List<Element> seenRemotes = new ArrayList<>();
+
+    @Override
+    public synchronized void init(final ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        Main.types = processingEnv.getTypeUtils();
+        Main.elements = processingEnv.getElementUtils();
+    }
+
+    @Override
+    public boolean process(final Set<? extends TypeElement> annotations,
+                           final RoundEnvironment roundEnv) {
+
+        if (!getBoolean("ts-exporter")) {
+            return false;
+        }
+
+        try {
+            process(roundEnv, annotations.stream().collect(toMap(identity(), roundEnv::getElementsAnnotatedWith)));
+            return false;
+        } catch (final Exception e) {
+            e.printStackTrace();
+            processingEnv.getMessager().printMessage(ERROR, "Error on TypeScript exporter.");
+            return false;
+        }
+    }
+
+    private void process(final RoundEnvironment roundEnv,
+                         final Map<TypeElement, Set<? extends Element>> typesByAnnotations) {
+
+        if (!roundEnv.processingOver() && !roundEnv.errorRaised()) {
+            typesByAnnotations.forEach((annotation, classes) -> {
+                if (REMOTE.equals(annotation.getQualifiedName().toString())) {
+                    seenRemotes.addAll(classes);
+                } else if (PORTABLE.equals(annotation.getQualifiedName().toString())) {
+                    seenPortables.addAll(classes);
+                } else if (ENTRY_POINT.equals(annotation.getQualifiedName().toString())) {
+                    System.out.println("EntryPoint detected.");
+                } else {
+                    throw new RuntimeException("Unsupported annotation type.");
+                }
+            });
+        } else {
+            writeExportFile(seenPortables, "portables.tsexporter");
+            writeExportFile(seenRemotes, "remotes.tsexporter");
+
+            if (!getBoolean("ts-exporter-generate")) {
+                System.out.println("TypeScript exporter will not run because ts-exporter-generate property is not set.");
+                return;
+            }
+
+            System.out.println("Generating TypeScript modules...");
+            long start = System.currentTimeMillis();
+            new TsCodegenExporter(readDecoratorFiles()).run();
+            System.out.println("TypeScript exporter has successfully run. (" + (System.currentTimeMillis() - start) + "ms)");
+        }
+    }
+
+    private Set<DecoratorImportEntry> readDecoratorFiles() {
+        return new HashSet<>(asList(
+                new DecoratorImportEntry("appformer", "decorators/PathDEC", "org.uberfire.backend.vfs.Path"),
+                new DecoratorImportEntry("appformer", "decorators/PathImplDEC", "org.uberfire.backend.vfs.PathFactory.PathImpl"),
+                new DecoratorImportEntry("appformer", "decorators/ObservablePathDEC", "org.uberfire.backend.vfs.ObservablePath"),
+                new DecoratorImportEntry("appformer", "decorators/ObservablePathImplDEC", "org.uberfire.backend.vfs.impl.ObservablePathImpl")
+        ));
+    }
+
+    private void writeExportFile(final List<Element> elements,
+                                 final String fileName) {
+
+        try {
+            System.out.println("Saving export file: " + fileName + "... ");
+            try (final Writer writer = processingEnv.getFiler().createResource(CLASS_OUTPUT, TS_EXPORTER_PACKAGE, fileName).openWriter()) {
+                writer.write(elements.stream().map(element -> ((TypeElement) element).getQualifiedName().toString()).distinct().collect(joining("\n")));
+            }
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ core/src/showcase-components/DemoReactComponent.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { ExampleList } from "appformer";
+
+export default class DemoComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0, lastMessage: "No messages were received yet.." };
+    this.props.onFoo(message => this.setState({ lastMessage: message }));
+  }
+
+  add() {
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+  }
+
+  subtract() {
+    this.setState(prevState => ({
+      count: Math.max(0, prevState.count - 1)
+    }));
+  }
+
+  render() {
+    return (
+      <div className="react-app-example">
+        <ExampleList name={"Random stuff"} id={this.props.number} />
+
+        <span>
+          <b>Count: </b>
+          {this.state.count}
+        </span>
+        <br />
+        <button className={"btn btn-primary btn-sm"} onClick={() => this.add()}>
+          {" "}
+          +
+        </button>
+        <button className={"btn btn-primary btn-sm"} onClick={() => this.subtract()}>
+          {" "}
+          -
+        </button>
+        <br />
+
+        <br />
+        <span>
+          <b>Last Message: </b>
+        </span>
+        <br />
+        <span>{this.state.lastMessage}</span>
+        <br />
+      </div>
+    );
+  }
+}
