dcd9c534d69f4a54a1d6c89ba175ba72219f578e
Caio Uvini <caiouvini@gmail.com>
2019-01-16 15:24:41 -0200

    add drools-wb home screen

--- packages/workbench-screens/src/index.ts
+++ packages/workbench-screens/src/index.ts
@@ -2,3 +2,4 @@ export * from './spaces';
 export * from './home-screen-api';
 export * from './home-perspective';
 export * from './stunner-showcase-home-screen';
+export * from  './drools-wb-home-screen';
