f5ba636d98ed1cffa37627570ee278ec8c33bd1d
Tiago Bento <tiagobentofernandes@gmail.com>
2019-01-28 14:09:35 -0200

    Formatting and finishing i18n labels

--- packages/workbench-screens/src/spaces-screen/NewSpacePopup.tsx
+++ packages/workbench-screens/src/spaces-screen/NewSpacePopup.tsx
@@ -1,110 +1,119 @@
 import * as React from "react";
 import * as AppFormer from "appformer-js";
-import {Popup} from "./Popup";
-import {OrganizationalUnitService} from "@kiegroup-ts-generated/uberfire-structure-api-rpc";
-import {AuthenticationService} from "@kiegroup-ts-generated/errai-security-server-rpc";
-import {UserImpl} from "@kiegroup-ts-generated/errai-security-server";
+import { Popup } from "./Popup";
+import { OrganizationalUnitService } from "@kiegroup-ts-generated/uberfire-structure-api-rpc";
+import { AuthenticationService } from "@kiegroup-ts-generated/errai-security-server-rpc";
+import { UserImpl } from "@kiegroup-ts-generated/errai-security-server";
 
 interface Props {
-    onClose: () => void
-    organizationalUnitService: OrganizationalUnitService,
-    authenticationService: AuthenticationService,
+  onClose: () => void;
+  organizationalUnitService: OrganizationalUnitService;
+  authenticationService: AuthenticationService;
 }
 
 interface State {
-    name: string;
-    errorMessages: string[];
-    displayedErrorMessages: string[];
+  name: string;
+  errorMessages: string[];
+  displayedErrorMessages: string[];
 }
 
 export class NewSpacePopup extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { name: "", errorMessages: [], displayedErrorMessages: [] };
+  }
 
-    constructor(props: Props) {
-        super(props);
-        this.state = {name: "", errorMessages: [], displayedErrorMessages: []};
-    }
+  private async add() {
+    const user = await this.props.authenticationService.getUser({});
 
-    private async add() {
+    const newSpace = {
+      name: this.state.name,
+      owner: (user as UserImpl).name!,
+      defaultGroupId: `com.${this.state.name.toLowerCase()}`
+    };
 
-        const user = await this.props.authenticationService.getUser({});
+    const emptyName = Promise.resolve().then(() => {
+      if (!newSpace.name || newSpace.name.trim() === "") {
+        this.addErrorMessage(AppFormer.translate("EmptyFieldValidation", ["Name"]));
+        return Promise.reject();
+      } else {
+        return Promise.resolve();
+      }
+    });
 
-        const newSpace = {
-            name: this.state.name,
-            owner: (user as UserImpl).name!,
-            defaultGroupId: `com.${this.state.name.toLowerCase()}`
-        };
+    const duplicatedName = Promise.resolve()
+      .then(() => this.props.organizationalUnitService.getOrganizationalUnit({ name: newSpace.name }))
+      .then(space => {
+        if (space) {
+          this.addErrorMessage(AppFormer.translate("DuplicatedOrganizationalUnitValidation", ["Space"]));
+          return Promise.reject();
+        } else {
+          return Promise.resolve();
+        }
+      });
 
-        const emptyName = Promise.resolve()
-            .then(() => {
-                if (!newSpace.name || newSpace.name.trim() === "") {
-                    this.addErrorMessage(AppFormer.translate("EmptyFieldValidation", ["Name"]));
-                    return Promise.reject();
-                } else {
-                    return Promise.resolve();
-                }
-            });
+    const validGroupId = Promise.resolve()
+      .then(() => this.props.organizationalUnitService.isValidGroupId({ proposedGroupId: newSpace.defaultGroupId }))
+      .then(valid => {
+        if (!valid) {
+          this.addErrorMessage(AppFormer.translate("InvalidSpaceName", []));
+          return Promise.reject();
+        } else {
+          return Promise.resolve();
+        }
+      });
 
-        const duplicatedName = Promise.resolve()
-            .then(() => this.props.organizationalUnitService.getOrganizationalUnit({name: newSpace.name}))
-            .then(space => {
-                if (space) {
-                    this.addErrorMessage(AppFormer.translate("DuplicatedOrganizationalUnitValidation", ["Space"]));
-                    return Promise.reject();
-                } else {
-                    return Promise.resolve();
-                }
-            });
+    this.setState({ errorMessages: [] }, () =>
+      Promise.resolve()
+        .then(() => Promise.all([emptyName, duplicatedName, validGroupId]))
+        .then(() => this.props.organizationalUnitService.createOrganizationalUnit0(newSpace))
+        .then(i => this.props.onClose())
+        .catch(() => this.setState(prevState => ({ displayedErrorMessages: prevState.errorMessages })))
+    );
+  }
 
-        const validGroupId = Promise.resolve()
-            .then(() => this.props.organizationalUnitService.isValidGroupId({proposedGroupId: newSpace.defaultGroupId}))
-            .then(valid => {
-                if (!valid) {
-                    this.addErrorMessage(AppFormer.translate("InvalidSpaceName", []));
-                    return Promise.reject();
-                } else {
-                    return Promise.resolve();
-                }
-            });
+  private addErrorMessage(msg: string) {
+    this.setState(prevState => ({ errorMessages: [...prevState.errorMessages, msg] }));
+  }
 
-        this.setState({errorMessages: []}, () => Promise.resolve()
-            .then(() => Promise.all([emptyName, duplicatedName, validGroupId]))
-            .then(() => this.props.organizationalUnitService.createOrganizationalUnit0(newSpace))
-            .then(i => this.props.onClose())
-            .catch(() => this.setState(prevState => ({displayedErrorMessages: prevState.errorMessages}))));
-    }
-
-    private addErrorMessage(msg: string) {
-        this.setState(prevState => ({errorMessages: [...prevState.errorMessages, msg]}));
-    }
-
-    public render() {
-        return <>
-
+  public render() {
+    return (
+      <>
         <Popup onClose={() => this.props.onClose()}>
-            <div className={"modal-content"}>
-                <div className={"modal-header"}>
-                    <button type="button" className={"close"} data-dismiss="modal" onClick={() => this.props.onClose()}>×</button>
-                    <h4 className={"modal-title"}>Add Space</h4>
-                </div>
-                <div className={"modal-body"}>
-                    {this.state.displayedErrorMessages.map(errorMessage =>
-                        <div key={errorMessage} className={"alert alert-danger alert-dismissable"}>
-                            <span className={"pficon pficon-error-circle-o"}/>
-                            <span>{errorMessage}</span>
-                        </div>
-                    )}
-                    <label className={"form-control-label required"}>Name</label>
-                    <div className={"form-group"}>
-                        <input type={"text"} className={"form-control"} onInput={(e: any) => this.setState({name: e.target.value})}/>
-                    </div>
-                </div>
-                <div className={"modal-footer"}>
-                    <button className={"btn btn-default"} onClick={() => this.props.onClose()}>Cancel</button>
-                    <button className={"btn btn-primary"} onClick={() => this.add()}>Add</button>
-                </div>
+          <div className={"modal-content"}>
+            <div className={"modal-header"}>
+              <button type="button" className={"close"} data-dismiss="modal" onClick={() => this.props.onClose()}>
+                ×
+              </button>
+              <h4 className={"modal-title"}>Add Space</h4>
             </div>
+            <div className={"modal-body"}>
+              {this.state.displayedErrorMessages.map(errorMessage => (
+                <div key={errorMessage} className={"alert alert-danger alert-dismissable"}>
+                  <span className={"pficon pficon-error-circle-o"} />
+                  <span>{errorMessage}</span>
+                </div>
+              ))}
+              <label className={"form-control-label required"}>Name</label>
+              <div className={"form-group"}>
+                <input
+                  type={"text"}
+                  className={"form-control"}
+                  onInput={(e: any) => this.setState({ name: e.target.value })}
+                />
+              </div>
+            </div>
+            <div className={"modal-footer"}>
+              <button className={"btn btn-default"} onClick={() => this.props.onClose()}>
+                {AppFormer.translate("Cancel", [])}
+              </button>
+              <button className={"btn btn-primary"} onClick={() => this.add()}>
+                {AppFormer.translate("Add", [])}
+              </button>
+            </div>
+          </div>
         </Popup>
-
-        </>;
-    }
+      </>
+    );
+  }
 }
--- packages/workbench-screens/src/spaces-screen/index.tsx
+++ packages/workbench-screens/src/spaces-screen/index.tsx
@@ -1,41 +1,45 @@
 import * as AppFormer from "appformer-js";
 import * as React from "react";
-import {SpacesScreen} from "./SpacesScreen";
-import {LibraryService} from "@kiegroup-ts-generated/kie-wb-common-library-api-rpc"
-import {ClusterLibraryEvent} from "@kiegroup-ts-generated/kie-wb-common-library-api"
-import {AuthenticationService} from "@kiegroup-ts-generated/errai-security-server-rpc"
-import {OrganizationalUnitService} from "@kiegroup-ts-generated/uberfire-structure-api-rpc"
-import {NewOrganizationalUnitEvent, RemoveOrganizationalUnitEvent} from "@kiegroup-ts-generated/uberfire-structure-api"
-import {PreferenceBeanServerStore} from "@kiegroup-ts-generated/uberfire-preferences-api-rpc";
+import { SpacesScreen } from "./SpacesScreen";
+import { LibraryService } from "@kiegroup-ts-generated/kie-wb-common-library-api-rpc";
+import { ClusterLibraryEvent } from "@kiegroup-ts-generated/kie-wb-common-library-api";
+import { AuthenticationService } from "@kiegroup-ts-generated/errai-security-server-rpc";
+import { OrganizationalUnitService } from "@kiegroup-ts-generated/uberfire-structure-api-rpc";
+import {
+  NewOrganizationalUnitEvent,
+  RemoveOrganizationalUnitEvent
+} from "@kiegroup-ts-generated/uberfire-structure-api";
+import { PreferenceBeanServerStore } from "@kiegroup-ts-generated/uberfire-preferences-api-rpc";
 
 export class SpacesScreenAppFormerComponent extends AppFormer.Screen {
+  private self: SpacesScreen;
 
-    private self: SpacesScreen;
+  constructor() {
+    super("LibraryOrganizationalUnitsScreen");
+    this.af_isReact = true;
+    this.af_componentTitle = "Spaces screen";
+    this.af_subscriptions = new Map<string, (e: any) => void>([
+      [NewOrganizationalUnitEvent.__fqcn(), (e: NewOrganizationalUnitEvent) => this.self.refreshSpaces()],
+      [RemoveOrganizationalUnitEvent.__fqcn(), (e: RemoveOrganizationalUnitEvent) => this.self.refreshSpaces()],
+      [ClusterLibraryEvent.__fqcn(), (e: ClusterLibraryEvent) => this.self.refreshSpaces()]
+    ]);
+  }
 
-    constructor() {
-        super("LibraryOrganizationalUnitsScreen");
-        this.af_isReact = true;
-        this.af_componentTitle = "Spaces screen";
-        this.af_subscriptions = new Map<string, (e: any) => void>([
-            [NewOrganizationalUnitEvent.__fqcn(), (e: NewOrganizationalUnitEvent) => this.self.refreshSpaces()],
-            [RemoveOrganizationalUnitEvent.__fqcn(), (e: RemoveOrganizationalUnitEvent) => this.self.refreshSpaces()],
-            [ClusterLibraryEvent.__fqcn(), (e: ClusterLibraryEvent) => this.self.refreshSpaces()],
-        ]);
-    }
+  public af_onOpen(): void {
+    this.self.refreshSpaces();
+  }
 
-    public af_onOpen(): void {
-        this.self.refreshSpaces();
-    }
-
-    public af_componentRoot(): AppFormer.Element {
-        return <SpacesScreen exposing={ref => this.self = ref()}
-                             libraryService={new LibraryService()}
-                             authenticationService={new AuthenticationService()}
-                             organizationalUnitService={new OrganizationalUnitService()}
-                             preferenceBeanServerStore={new PreferenceBeanServerStore()}/>;
-
-    }
-
+  public af_componentRoot(): AppFormer.Element {
+    return (
+      <SpacesScreen
+        exposing={ref => (this.self = ref())}
+        libraryService={new LibraryService()}
+        authenticationService={new AuthenticationService()}
+        organizationalUnitService={new OrganizationalUnitService()}
+        preferenceBeanServerStore={new PreferenceBeanServerStore()}
+      />
+    );
+  }
 }
 
 AppFormer.register(new SpacesScreenAppFormerComponent());
